e53c2865c259b306d9ef96c8c98e8043e9bbad99
Stephen <stephen.lyssy@gmail.com>
2022-08-08 21:54:35 -0500

    Created the 3 additional fetch requests, fetch 5 posts, fetch comments and fetch users.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # fetch-practice-1
+
+-[x] Create a new repo in your GitHub called FetchPractice-1
+
+-[x] Clone it into your devFolder
+
+-[x] Inside the new repo create two files: index.html and main.js
+
+-[x] Copy/paste the code below into the respective files
+
+-[x] While working throughout this fetch practice make sure you reference and read in its entirety the MDN Docs on fetch()
+
+-[ ] Using the documents at json placeholder, for each of the buttons listed in the index.html file:
+
+-[ ] Create a function that fetches the data the button should fetch
+
+-[ ] Push the response into an array: arrayOfPosts
+
+-[ ] Display them as an li in a ul
+
+-[ ] Using the docs on jsonplaceholder, post a new post to the database.
+
+-[ ] Using the docs, put (edit) a post.
+
+-[ ] Get this all finished before coming into class.
+
+-[ ] If you don't you will be very unprepared
---
+++ main.js
@@ -0,0 +1,88 @@
+let arrayOfPosts;
+let fivePosts;
+let arrayOfComments;
+let arrayOfUserIds;
+// This function waits for the web page to be loaded, when it does it will run the code inside of it which happens to be getPosts()
+window.onload = function () {
+  getPosts();
+  getFivePosts();
+  getComments();
+  getUsersIds();
+};
+
+// This function is going to make a fetch request to the URL inside its parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
+const getPosts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((posts) => (arrayOfPosts = posts));
+};
+
+// This function logs the results in your browser's console
+const consolePosts = () => {
+  console.log(arrayOfUserIds);
+};
+
+// this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
+const displayPost = () => {
+  const allPosts = document.getElementById('all-posts');
+  arrayOfPosts.map((post, index) => {
+    const li = document.createElement('li');
+    const text = document.createTextNode(
+      `#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`
+    );
+    li.appendChild(text);
+    allPosts.append(li);
+  });
+};
+
+//* Fetch 5 Posts
+const getFivePosts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((posts) => (fivePosts = posts.slice(0, 5)));
+};
+
+const displayFivePost = () => {
+  const showFivePosts = document.getElementById('five-posts');
+  fivePosts.map((post, index) => {
+    const li = `<li>#${index + 1}, Title: ${post.title}: ${
+      post.body
+    }, by user: ${post.userId}</li>`;
+    showFivePosts.insertAdjacentHTML('beforeend', li);
+  });
+};
+
+// * Fetch Comments
+const getComments = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((posts) => (arrayOfComments = posts.map((post) => post.body)));
+};
+
+const displayComments = () => {
+  const showComments = document.getElementById('comments');
+  arrayOfComments.map((comment, index) => {
+    const li = `<li>#${index + 1}, Comment: ${comment}</li>`;
+    showComments.insertAdjacentHTML('beforeend', li);
+  });
+};
+
+const getUsersIds = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((posts) => (arrayOfUserIds = posts.map((post) => post.userId)));
+};
+
+const displayUserIds = () => {
+  const showUserIds = document.getElementById('user-id');
+  arrayOfUserIds.map((userId, index) => {
+    const li = `<li>#${index + 1}, User ID: ${userId}</li>`;
+    showUserIds.insertAdjacentHTML('beforeend', li);
+  });
+};
+/* 
+Your job now is to follow the functions above and use them as templates 
+ to build the functionality the buttons in the index.html file already 
+ have laid out in it. This way you can learn how to build fetch requests 
+ and work with other APIs and become a real developer!! 
+*/
